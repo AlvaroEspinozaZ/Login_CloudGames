a4dc0c59e180dc37c01fc5d8376783e57c5124b2
AlvaroEspinozaZ <130480207+AlvaroEspinozaZ@users.noreply.github.com>
2023-10-25 11:14:36 -0500

    UploadProyect

---
+++ Assets/CreateUser.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateUser : MonoBehaviour
+{
+    public string name;
+    public string lastname;
+    public string sex;
+    public string address;
+    public string dni;
+    public string cellphone;
+    public string username;
+    public string password;
+}
